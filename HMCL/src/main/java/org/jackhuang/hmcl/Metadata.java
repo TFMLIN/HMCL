983c2c019991476de65131b62c3dd7fcd4b6af4c
TFMLin <991307709@qq.com>
2023-05-29 00:56:24 +0800

    修改标题栏

--- HMCL/src/main/java/org/jackhuang/hmcl/Metadata.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/Metadata.java
@@ -31,8 +31,9 @@ public final class Metadata {
     private Metadata() {}
 
     public static final String NAME = "HMCL";
-    public static final String FULL_NAME = "Hello Minecraft! Launcher";
-    public static final String VERSION = System.getProperty("hmcl.version.override", JarUtils.getManifestAttribute("Implementation-Version", "@develop@"));
+    public static final String FULL_NAME = "哈理工MC启动器";
+//    public static final String VERSION = System.getProperty("hmcl.version.override", JarUtils.getManifestAttribute("Implementation-Version", "@develop@"));
+public static final String VERSION = "_Test0.0.1";
 
     public static final String TITLE = NAME + " " + VERSION;
     public static final String FULL_TITLE = FULL_NAME + " v" + VERSION;
